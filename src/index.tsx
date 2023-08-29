0db4068c478abb183dde9d854205a315d55af55d
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-29 16:32:55 +0200

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+
+// CSS
+import "./css/styles_variables.css"
+import "./css/styles_main.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+
+);
+
